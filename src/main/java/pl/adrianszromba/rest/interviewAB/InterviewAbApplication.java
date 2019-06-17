d5d52f2c0e8e6d6f0359d8af328c327b0aeae202
adrianszromba1 <adrian.szromba1@gmail.com>
2019-06-17 19:32:54 +0200

    init commit

---
+++ src/main/java/pl/adrianszromba/rest/interviewAB/InterviewAbApplication.java
@@ -0,0 +1,13 @@
+package pl.adrianszromba.rest.interviewAB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterviewAbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InterviewAbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/adrianszromba/rest/interviewAB/InterviewAbApplicationTests.java
@@ -0,0 +1,16 @@
+package pl.adrianszromba.rest.interviewAB;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class InterviewAbApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
